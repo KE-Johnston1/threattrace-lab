a2766e814439078e674351473a7d6360e0bd2a13
Karen Johnstron <228061156+KE-Johnston1@users.noreply.github.com>
2025-10-06 20:09:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ def menu():
     print("\nThreatTrace Lab Launcher")
     print("------------------------")
     print("[1] Simulate SSH brute force attack")
-    print("[2] Parse logs for suspicious activity")
+    print("[2] Parse brute force logs")
     print("[3] Build incident timeline")
     print("[4] Extract IOCs")
     print("[5] View incident report")
+    print("[6] Parse honeypot activity")
     print("[0] Exit")
 
 def run_module(choice):
-    if choice == "1":
-        os.system("python offensive-simulation/brute_force_simulator.py")
-    elif choice == "2":
-        os.system("python defensive-detection/log_parser.py")
-    elif choice == "3":
-        os.system("python analyst-investigation/timeline_builder.py")
-    elif choice == "4":
-        os.system("python analyst-investigation/ioc_extractor.py")
-    elif choice == "5":
-        os.system("code analyst-investigation/incident_report.md")  # Opens in VS Code if installed
+    modules = {
+        "1": "python offensive-simulation/brute_force_simulator.py",
+        "2": "python defensive-detection/log_parser.py",
+        "3": "python analyst-investigation/timeline_builder.py",
+        "4": "python analyst-investigation/ioc_extractor.py",
+        "5": "code analyst-investigation/incident_report.md",  # Opens in VS Code if installed
+        "6": "python defensive-detection/honeypot_parser.py"
+    }
+
+    if choice in modules:
+        os.system(modules[choice])
     elif choice == "0":
         print("Exiting ThreatTrace Lab.")
     else:
@@ -33,3 +35,4 @@ if __name__ == "__main__":
         if choice == "0":
             break
         run_module(choice)
+
